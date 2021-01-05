b82ce76e7153519a0ca00953779593bfcf899d1a
iamsantoshpro <santosh.bm302@gmail.com>
2021-01-05 22:55:19 +0530

    Updates

---
+++ precache-manifest.7d94ce41638d1d0118788fdbed7e7d4b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b07e1c6684fa4106874dae7f3576071",
+    "url": "/sb/index.html"
+  },
+  {
+    "revision": "5c1c633222eaad441995",
+    "url": "/sb/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3a1c226f66da345ba5e9",
+    "url": "/sb/static/css/main.5d7e3441.chunk.css"
+  },
+  {
+    "revision": "5c1c633222eaad441995",
+    "url": "/sb/static/js/2.33f2b88c.chunk.js"
+  },
+  {
+    "revision": "1922274b530f5a40982d1bd4f55fc6b2",
+    "url": "/sb/static/js/2.33f2b88c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a1c226f66da345ba5e9",
+    "url": "/sb/static/js/main.ace3ee26.chunk.js"
+  },
+  {
+    "revision": "b5eb657b3999fbcfccdc",
+    "url": "/sb/static/js/runtime-main.1101aae5.js"
+  },
+  {
+    "revision": "e86ee813ada1c4fd90b165ae84b5e56c",
+    "url": "/sb/static/media/Logo.e86ee813.png"
+  },
+  {
+    "revision": "af555c36b5a70d3579882d84c526617e",
+    "url": "/sb/static/media/banner3.af555c36.jpg"
+  },
+  {
+    "revision": "0ac3393fa8c106597e2f70baf4ed7800",
+    "url": "/sb/static/media/bg2.0ac3393f.jpg"
+  },
+  {
+    "revision": "691f41597294d4570ecb790b9b79e928",
+    "url": "/sb/static/media/catering-2.691f4159.jpeg"
+  },
+  {
+    "revision": "69cc9d4a77276652060bd085e729e9c3",
+    "url": "/sb/static/media/events-.69cc9d4a.jpg"
+  },
+  {
+    "revision": "8eb68a3f9ccdf709913b6fdd4a0eee68",
+    "url": "/sb/static/media/snacks.8eb68a3f.jpg"
+  },
+  {
+    "revision": "b7705a15df72e8d4afc709a88f42648f",
+    "url": "/sb/static/media/sweets.b7705a15.jpg"
+  }
+]);
